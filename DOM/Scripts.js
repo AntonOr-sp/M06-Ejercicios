9d0d6186acc6a87ca6c1d2e463d016a4e06a0ffb
Anton Orlov ITB <anton.orlov.7e8@itb.cat>
2026-03-22 21:59:52 +0100

    Alfa vertion
    Añadi zoom a objetos

--- DOM/Scripts.js
+++ DOM/Scripts.js
@@ -130,7 +130,7 @@ function comprobar() {
     }
 }
 
-let palabra = "";
+let palabra = "NADAL";
 function palabraSecreta() {
     fetch('https://random-word-api.herokuapp.com/word?lang=es&length=5')
         .then(response => response.json())
